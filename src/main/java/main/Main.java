4f32225149b311612cb6b0a65f7875591599270a
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-09-18 19:21:09 +0400

    SignOut added

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import frontend.Frontend;
 import frontend.SignInServlet;
+import frontend.SignOutServlet;
 import frontend.SignUpServlet;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -28,14 +29,16 @@ public class Main {
         AccountService accountService = new AccountService();
 
         Servlet frontend = new Frontend();
-        Servlet signup = new SignUpServlet(accountService);
-        Servlet signin = new SignInServlet(accountService);
+        Servlet signUp = new SignUpServlet(accountService);
+        Servlet signIn = new SignInServlet(accountService);
+        Servlet signOut = new SignOutServlet(accountService);
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(frontend), "/api/v1/auth/signin");
-        context.addServlet(new ServletHolder(signup), "/signup");
-        context.addServlet(new ServletHolder(signin), "/signin");
+        context.addServlet(new ServletHolder(signUp), "/signup");
+        context.addServlet(new ServletHolder(signIn), "/signin");
+        context.addServlet(new ServletHolder(signOut), "/signout");
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
