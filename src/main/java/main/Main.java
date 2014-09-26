686524353d1b8d947debd78a312d85c5e2e71bad
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-09-26 20:48:59 +0400

    Изменил Main в соответствии с ТЗ

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import frontend.Frontend;
 import frontend.SignInServlet;
 import frontend.SignOutServlet;
 import frontend.SignUpServlet;
@@ -18,30 +17,29 @@ import javax.servlet.Servlet;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        int port = 8080;
-        if (args.length == 1) {
-            String portString = args[0];
-            port = Integer.valueOf(portString);
+        if (args.length != 1) {
+            System.out.append("Use port as the first argument");
+            System.exit(1);
         }
 
-        System.out.append("Starting at port: ").append(String.valueOf(port)).append('\n');
+        String portString = args[0];
+        int port = Integer.valueOf(portString);
+        System.out.append("Starting at port: ").append(portString).append('\n');
 
         AccountService accountService = new AccountService();
 
-        Servlet frontend = new Frontend();
         Servlet signUp = new SignUpServlet(accountService);
         Servlet signIn = new SignInServlet(accountService);
         Servlet signOut = new SignOutServlet(accountService);
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(frontend), "/api/v1/auth/signin");
         context.addServlet(new ServletHolder(signUp), "/signup");
         context.addServlet(new ServletHolder(signIn), "/signin");
         context.addServlet(new ServletHolder(signOut), "/signout");
 
         ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setDirectoriesListed(true);
+//        resource_handler.setDirectoriesListed(true);
         resource_handler.setResourceBase("static");
 
         HandlerList handlers = new HandlerList();
