c3b27a739a2786182ae8f9ea0ce43b166592d18c
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-10-24 17:59:34 +0400

    Игровая сессия

--- src/main/java/mechanics/GameSession.java
+++ src/main/java/mechanics/GameSession.java
@@ -1,8 +1,41 @@
 package mechanics;
 
+import base.UserGameState;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Created by titaevskiy.s on 24.10.14
  */
 public class GameSession {
+    private final GameUser first;
+    private final GameUser second;
 
+    private int whoseTurn;
+    private int[] field = new int[9];
+
+    private Map<String, GameUser> loginToGameUser = new HashMap<>();
+
+    public GameSession(String loginFirst, String loginSecond) {
+        Random random = new Random();
+        int signFirst = (random.nextInt(1) == 0) ? GameUser.X : GameUser.O;
+        int signSecond = (GameUser.X + GameUser.O) - signFirst;
+
+        first = new GameUser(loginFirst, signFirst);
+        second = new GameUser(loginSecond, signSecond);
+
+        loginToGameUser.put(loginFirst, first);
+        loginToGameUser.put(loginSecond, second);
+
+        whoseTurn = GameUser.X;
+    }
+
+    public UserGameState getUserGameState(String login) {
+        GameUser myGameUser = loginToGameUser.get(login);
+        GameUser enemyGameUser = (myGameUser == first) ? second : first;
+
+        return new UserGameState(myGameUser, enemyGameUser, whoseTurn, field);
+    }
 }
