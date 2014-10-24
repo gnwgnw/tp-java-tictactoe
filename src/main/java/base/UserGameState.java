7f4cf1e485ed95a4b470d35f6a3fd30cc9729ca0
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-10-24 17:56:33 +0400

    Текущая игровая ситуация для отправки на клиент

--- src/main/java/base/UserGameState.java
+++ src/main/java/base/UserGameState.java
@@ -1,8 +1,36 @@
 package base;
 
+import mechanics.GameUser;
+
 /**
  * Created by titaevskiy.s on 24.10.14
  */
-public interface UserGameState {
-    String getMyLogin();
+public class UserGameState {
+    private final GameUser myGameUser;
+    private final GameUser enemyGameUser;
+    private final int whoseTurn;
+    private final int[] field;
+
+    public UserGameState(GameUser myGameUser, GameUser enemyGameUser, int whoseTurn, int[] field) {
+        this.myGameUser = myGameUser;
+        this.enemyGameUser = enemyGameUser;
+        this.whoseTurn = whoseTurn;
+        this.field = field;
+    }
+
+    public GameUser getMyGameUser() {
+        return myGameUser;
+    }
+
+    public GameUser getEnemyGameUser() {
+        return enemyGameUser;
+    }
+
+    public int getWhoseTurn() {
+        return whoseTurn;
+    }
+
+    public int[] getField() {
+        return field;
+    }
 }
