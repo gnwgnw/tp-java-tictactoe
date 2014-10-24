738668c628958c18f4f4dfe471cb96f595540715
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-10-24 17:53:59 +0400

    Игровая информация пользователя

--- src/main/java/mechanics/GameUser.java
+++ src/main/java/mechanics/GameUser.java
@@ -3,4 +3,23 @@ package mechanics;
 /**
  * Created by titaevskiy.s on 24.10.14
  */
-public class GameUser {}
+public class GameUser {
+    public static final int X = 1;
+    public static final int O = 4;
+
+    private final String login;
+    private final int sign;
+
+    public GameUser(String login, int sign) {
+        this.login = login;
+        this.sign = sign;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public int getSign() {
+        return sign;
+    }
+}
