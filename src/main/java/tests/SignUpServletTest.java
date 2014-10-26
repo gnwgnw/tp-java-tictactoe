aaf3b4dc083038eb323d0cd648a5098cec758a0d
Marina <marina@marina-elementary-os.(none)>
2014-10-26 14:54:18 +0400

    Тесты к 1-му РК.

--- src/main/java/tests/SignUpServletTest.java
+++ src/main/java/tests/SignUpServletTest.java
@@ -11,45 +11,52 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class SignUpServletTest {
     final AccountService accountService = mock(AccountService.class);
-    //TODO possible?!
     final SignUpServlet signUpServlet = new SignUpServlet(accountService);
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    HttpServletResponse response = mock(HttpServletResponse.class);
+    final HttpServletRequest request = mock(HttpServletRequest.class);
+    final HttpServletResponse response = mock(HttpServletResponse.class);
+    final StringWriter stringWriter = new StringWriter();
+    final PrintWriter printWriter = new PrintWriter(stringWriter);
+
+    String newLoginString = "testUser";
+    String newEmailString = "testUser@mail.ru";
+    String newPasswordString = "123";
+
+    String oldLoginString = "defaultUser1";
+    String oldEmailString = "defaultUser1@mail.ru";
+    String oldPasswordString = "123";
 
     @Test
     public void testDoPostOK() throws Exception {
-        when(request.getParameter("login")).thenReturn("testUser");
-        when(request.getParameter("email")).thenReturn("testUser@mail.ru");
-        when(request.getParameter("password")).thenReturn("123");
-
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(request.getParameter("login")).thenReturn(newLoginString);
+        when(request.getParameter("email")).thenReturn(newEmailString);
+        when(request.getParameter("password")).thenReturn(newPasswordString);
         when(response.getWriter()).thenReturn(printWriter);
-        when(accountService.signUp("testUser", "testUser@mail.ru", "123")).thenReturn(ResponsesCode.OK);
+        when(accountService.signUp(newLoginString, newEmailString, newPasswordString)).thenReturn(ResponsesCode.OK);
 
         signUpServlet.doPost(request, response);
-//        assertEquals(stringWriter.toString(), "The user testUser created");
+        verify(accountService, atLeastOnce()).signUp(newLoginString, newEmailString, newPasswordString);
         assertTrue(stringWriter.toString().contains("The user testUser created"));
     }
 
     @Test
     public void testDoPostAlreadyExist() throws Exception {
-        when(request.getParameter("login")).thenReturn("defaultUser1");
-        when(request.getParameter("email")).thenReturn("defaultUser1@mail.ru");
-        when(request.getParameter("password")).thenReturn("123");
+        when(request.getParameter("login")).thenReturn(oldLoginString);
+        when(request.getParameter("email")).thenReturn(oldEmailString);
+        when(request.getParameter("password")).thenReturn(oldPasswordString);
 
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);
-        when(accountService.signUp("defaultUser1", "defaultUser1@mail.ru", "123")).thenReturn(ResponsesCode.ALREADY_EXISTS);
+        when(accountService.signUp(oldLoginString, oldEmailString, oldPasswordString)).thenReturn(ResponsesCode.ALREADY_EXISTS);
 
         signUpServlet.doPost(request, response);
-//        assertEquals(stringWriter.toString(), "The user testUser created");
+        verify(accountService, atLeastOnce()).signUp(oldLoginString, oldEmailString, oldPasswordString);
         assertTrue(stringWriter.toString().contains("The user defaultUser1 already exists"));
     }
+
+    //TODO unknown error...?
 }
--- src/main/java/tests/UserPageServletTest.java
+++ src/main/java/tests/UserPageServletTest.java
@@ -1,28 +1,56 @@
 package tests;
 
 import base.AccountService;
+import base.UserProfile;
 import org.junit.Test;
+import servlets.SignInServlet;
 import servlets.UserPageServlet;
+import utils.PageGenerator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+
 public class UserPageServletTest {
     final AccountService accountService = mock(AccountService.class);
-    //TODO possible?!
     final UserPageServlet userPageServlet = new UserPageServlet(accountService);
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    HttpServletResponse response = mock(HttpServletResponse.class);
+    final HttpServletRequest request = mock(HttpServletRequest.class);
+    final HttpServletResponse response = mock(HttpServletResponse.class);
+    final HttpSession httpSession = mock(HttpSession.class);
+    final StringWriter stringWriter = new StringWriter();
+    final PrintWriter printWriter = new PrintWriter(stringWriter);
+
+    String loginString = "defaultUser1";
+    String emailString = "defaultUser1@mail.ru";
+    String passwordString = "123";
+    String sessionString = "session";
+
+    final UserProfile userProfile = new UserProfile(loginString, emailString, passwordString);
 
     @Test
     public void testDoGet() throws Exception {
-        accountService.signIn("defaultUser1", "123", "session");
-        when(request.getSession().getId()).thenReturn("session");
-        //TODO WTF?!
-//        userPageServlet.doGet(request, response);
-        //TODO assert. Read local variable or read return value of response method...?
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("login", loginString);
+        pageVariables.put("email", emailString);
+
+        when(request.getSession()).thenReturn(httpSession);
+        when(httpSession.getId()).thenReturn(sessionString);
+        when(accountService.getUserProfile(anyString())).thenReturn(userProfile);
+        when(response.getWriter()).thenReturn(printWriter);
+
+        userPageServlet.doGet(request, response);
+
+        assertTrue(stringWriter.toString().contains(PageGenerator.getPage("userpage.tml", pageVariables)));
     }
 }
