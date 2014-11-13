17a7088ab26a7a53735ee8515beb309755357191
Kic <kic@kic-Lenovo-B450.(none)>
2014-11-13 13:33:28 +0400

    Новый пользователь базы

--- src/main/java/dao/dbSessionFactory.java
+++ src/main/java/dao/dbSessionFactory.java
@@ -16,7 +16,7 @@ public class dbSessionFactory {
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/tic_tac_db");
-        configuration.setProperty("hibernate.connection.username", "forum_user");
+        configuration.setProperty("hibernate.connection.username", "tic_tac_user");
         configuration.setProperty("hibernate.connection.password", "0");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", action);
