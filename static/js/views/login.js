b12eaa162b42e5eba453fe597286b0f9ddede642
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-11-30 19:10:58 +0300

    Страница логина

--- static/js/views/login.js
+++ static/js/views/login.js
@@ -1,37 +1,63 @@
 define([
-    'backbone',
-    'tmpl/login'
+        'backbone',
+        'models/user',
+        'tmpl/login'
     ],
     function(
         Backbone,
+        user,
         tmpl
     )
-{
-    var View = Backbone.View.extend({
+    {
+        var View = Backbone.View.extend({
 
-        template: tmpl,
-        initialize: function () {
-            this.render();            
-        },
-        render: function () {            
-            this.$el.html(this.template);
-            return this;
-        },
-        events: {
-            "show" : "show"
-        },
-        show: function () {
-            this.$el.css({'display':'block'});
-            if (!$('#login').html()) {
-                $('#login').html(this.$el);                
-            }            
-            this.trigger("show", this);
-        },
-        hide: function () {            
-            this.$el.css({'display':'none'})
-        }
+            id: "login",
+            template: tmpl,
+            model: user,
 
+            events: {
+                "submit": "login"
+            },
+
+            initialize: function () {
+                this.render();
+            },
+
+            render: function () {
+                this.$el.html(this.template());
+                return this;
+            },
+
+            show: function () {
+                this.$el.show();
+            },
+
+            hide: function () {
+                this.$el.hide();
+            },
+
+            login: function (event) {
+                event.preventDefault();
+
+                var url = "/login";
+                var data = this.$el.find('form').serializeObject();
+
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    dataType: "json",
+                    data: data,
+                    success: function (data) {
+                        //TODO
+                        console.log(data);
+                    },
+                    failure: function (data) {
+                        //TODO
+                        console.log(data);
+                    }
+                });
+            }
+        });
+
+        return new View();
     });
-
-    return new View();
-});
