eef03030e73972a140fbf2120b9333febeed8e21
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-11-30 19:09:00 +0300

    Реформат views/user

--- static/js/models/user.js
+++ static/js/models/user.js
@@ -1,50 +1,50 @@
 define([
         'backbone'
-	],
-	function(
+    ],
+    function(
         Backbone
-	)
-{
-    var Model = Backbone.Model.extend({
+    )
+    {
+        var Model = Backbone.Model.extend({
 
-        defaults: function () {
-            //TODO
-            this.set({
-                name: "",
-                gameCount: 0,
-                gameLose: 0,
-                gameWin: 0,
-                isLogined: false
-            });
-        },
+            defaults: function () {
+                //TODO
+                this.set({
+                    name: "",
+                    gameCount: 0,
+                    gameLose: 0,
+                    gameWin: 0,
+                    isLogined: false
+                });
+            },
 
-        isLogined: function () {
-            return this.get('isLogined');
-        },
+            isLogined: function () {
+                return this.get('isLogined');
+            },
 
-        login: function () {
-            //TODO
-            this.testLogin();
-        },
+            login: function () {
+                //TODO
+                this.testLogin();
+            },
 
-        logout: function () {
-            //TODO
-            this.defaults();
-            console.log(this.toJSON());
-        },
+            logout: function () {
+                //TODO
+                this.defaults();
+                console.log(this.toJSON());
+            },
 
-        //TODO testOnly
-        testLogin: function () {
-            this.set({
-                name: "Gex",
-                gameCount: 5,
-                gameLose: 1,
-                gameWin: 3,
-                isLogined: true
-            });
-            console.log(this.toJSON());
-        }
+            //TODO testOnly
+            testLogin: function () {
+                this.set({
+                    name: "Gex",
+                    gameCount: 5,
+                    gameLose: 1,
+                    gameWin: 3,
+                    isLogined: true
+                });
+                console.log(this.toJSON());
+            }
+        });
+
+        return new Model();
     });
-
-    return new Model();
-});
