c53348f213879cbf69e0090685d954a0f946fdb5
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-11-30 19:15:45 +0300

    Роуты на страницы логина и регистрации

--- static/js/router.js
+++ static/js/router.js
@@ -8,29 +8,38 @@ define([
         page_top,
         page_body
     )
-{
-    var Router = Backbone.Router.extend({
-        initialize: function() {
-	},
-        routes: {
-            'scoreboard': 'scoreboardAction',
-            'game': 'gameAction',
-            'login': 'loginAction',
-            'singup': 'signupAction',
-            '*default': 'menuActions'
-        },
-        menuActions: function () {
-            page_body.show('menu');
-        },
-        scoreboardAction: function () {
-        },
-        gameAction: function () {
-        },
-        loginAction: function () {            
-        },
-        signupAction: function () {
-        }
-    });
+    {
+        var Router = Backbone.Router.extend({
 
-    return new Router();
-});
+            initialize: function() {
+            },
+
+            routes: {
+                'scoreboard': 'scoreboardAction',
+                'game': 'gameAction',
+                'login': 'loginAction',
+                'signup': 'signupAction',
+                '*default': 'menuActions'
+            },
+
+            menuActions: function () {
+                page_body.show('menu');
+            },
+
+            scoreboardAction: function () {
+            },
+
+            gameAction: function () {
+            },
+
+            loginAction: function () {
+                page_body.show('login');
+            },
+
+            signupAction: function () {
+                page_body.show('signup');
+            }
+        });
+
+        return new Router();
+    });
