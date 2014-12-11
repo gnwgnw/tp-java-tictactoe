96c05e88e18d2258094aa38f45cb38212b54900e
Сергей Титаевский <Titaevskiy.S@gmail.com>
2014-12-11 16:36:19 +0300

    Выпилил run из интерфейса GM

--- src/main/java/mechanics/GameMechanics.java
+++ src/main/java/mechanics/GameMechanics.java
@@ -6,8 +6,6 @@ import messageSystem.Abonent;
  * Created by titaevskiy.s on 23.10.14
  */
 public interface GameMechanics extends Abonent, Runnable {
-    void run();
-
     void waitForEnemy(String login);
 
     void doTurn(String login, int position);
